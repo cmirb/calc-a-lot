293a1dace05a7f6f5b363f8df96fde996ec5b64d
cmirb <51628604+cmirb@users.noreply.github.com>
2023-06-05 13:36:46 +0200

    bug fixes

--- script.js
+++ script.js
@@ -1,84 +1,133 @@
 // basic math functions
 function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  if (b === 0) {
-    console.error('Cannot divide by zero!');
-    return 'Error';
+    return a + b;
   }
-  return a / b;
-}
-
-// calculator operation variables
-let firstNumber = 0;
-let operator = null;
-let secondNumber = 0;
-
-// operate functions
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      console.error('Cannot divide by zero!');
+      return 'Error';
+    }
+    return a / b;
+  }
+  
+  // calculator operation variables
+  let firstNumber = '';
+  let operator = null;
+  let secondNumber = '';
+  let waitForSecondNumber = false;
+  
+// operate function
 function operate(operator, a, b) {
-  switch (operator) {
-    case '+':
-      return add(a, b);
-    case '-':
-      return subtract(a, b);
-    case '*':
-      return multiply(a, b);
-    case '/':
-      return divide(a, b);
-    default:
-      return null;
+    if (operator === '÷' && b === 0) {
+      console.error('Cannot divide by zero!');
+      return 'Error: Cannot divide by zero!';
+    }
+    switch (operator) {
+      case '+':
+        return add(a, b);
+      case '-':
+        return subtract(a, b);
+      case '×':
+        return multiply(a, b);
+      case '÷':
+        return divide(a, b);
+      default:
+        return null;
+    }
   }
-}
-
-// populate display
-let display = document.getElementById('display');
-
-// Update display
-function updateDisplay(value) {
-  display.innerText = value;
-}
-
-// Attach click event to digit buttons
-let digitButtons = document.getElementsByClassName('digit');
-for (let button of digitButtons) {
-  button.addEventListener('click', function() {
-    display.innerText += this.innerText;
-  });
-}
-
-
+  
+  // populate display
+  let display = document.getElementById('display');
+  
+  // Update display
+  function updateDisplay(value) {
+    display.innerText = value;
+  }
+  
+  // Attach click event to digit buttons
+  let digitButtons = document.getElementsByClassName('digit');
+  for (let button of digitButtons) {
+    button.addEventListener('click', function() {
+      if (waitForSecondNumber) {
+        display.innerText = '';
+        waitForSecondNumber = false;
+      }
+      if (display.innerText === '0') {
+        display.innerText = this.innerText;
+      } else {
+        display.innerText += this.innerText;
+      }
+    });
+  }
+  
+  // Attach click event to operator buttons
 // calculator logic
 let operatorButtons = document.getElementsByClassName('operator');
 for (let button of operatorButtons) {
   button.addEventListener('click', function() {
-    firstNumber = parseFloat(display.innerText);
+    if (firstNumber !== '' && operator !== null && !waitForSecondNumber) {
+      secondNumber = parseFloat(display.innerText);
+      if (operator === '÷' && secondNumber === 0) {
+        updateDisplay('Error: Cannot divide by zero!');
+        return;
+      }
+      firstNumber = operate(operator, firstNumber, secondNumber);
+      updateDisplay(firstNumber);
+    } else {
+      firstNumber = parseFloat(display.innerText);
+    }
     operator = this.innerText;
-    display.innerText = '';
+    waitForSecondNumber = true;
   });
 }
 
-document.getElementById('equals').addEventListener('click', function() {
-  secondNumber = parseFloat(display.innerText);
-  let result = operate(operator, firstNumber, secondNumber);
-  updateDisplay(result.toFixed(2)); // round result to 2 decimal places
-  firstNumber = result; // store result for chained operations
-});
 
-// divide by zero 
-document.getElementsByClassName('clear')[0].addEventListener('click', function() {
-    firstNumber = 0;
+  
+  // Attach click event to equals button
+  document.getElementById('equals').addEventListener('click', function() {
+    if (operator !== null) {
+      let secondNumber = parseFloat(display.innerText);
+      if (operator === '÷' && secondNumber === 0) {
+        updateDisplay('Error: Cannot divide by zero!');
+      } else {
+        let result = operate(operator, firstNumber, secondNumber);
+        if (result === 'Error') {
+          updateDisplay('Error: Cannot divide by zero!');
+        } else {
+          if (Number.isInteger(result)) {
+            updateDisplay(result);
+          } else {
+            updateDisplay(result.toFixed(2)); // round result to 2 decimal places
+          }
+          firstNumber = result; // store result for chained operations
+        }
+      }
+      waitForSecondNumber = true;
+    }
+  });
+  
+  
+  
+  // Attach click event to clear button
+  document.getElementsByClassName('clear')[0].addEventListener('click', function() {
+    firstNumber = '';
     operator = null;
-    secondNumber = 0;
+    secondNumber = '';
+    waitForSecondNumber = false;
     updateDisplay(0);
+  });
+  
+  // Attach click event to delete button
+  document.getElementsByClassName('delete')[0].addEventListener('click', function() {
+    display.innerText = display.innerText.slice(0, -1);
   });
   
